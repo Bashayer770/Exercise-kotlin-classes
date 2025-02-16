76bd66d89c1dc1041117e625eb53e13b7f772e5f
Bashayer770 <Beshow95@gmail.com>
2025-02-16 18:57:40 +0300

    Classes

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,24 @@
 fun main() {
-    println("Hello World!")
+
+var book1= Book("The Hobbit",  "Robert",2005, genre = "Fantasy")
+
+book1.getBookInfo()
+
+
+
+//    println(book1.title + " " + book1.author+ "  " + book1.yearPublished)
+//    println("Book: ${book1.title}")
+}
+
+
+
+class Book(var title: String,var author: String,var yearPublished: Int, var genre: String) {
+
+
+    fun getBookInfo(){
+        println("Title: $title , Author: $author, yearPublished: $yearPublished, genre: $genre")
+    }
+//    var title: String = ""
+//    var author: String = ""
+//    var yearPublished: Int = 1000
 }
